be56d28a7ed65c4283b98ff1d8600aabbf90e292
yanuarultfah <46949146+yanuarultfah@users.noreply.github.com>
2022-03-27 21:32:15 +0700

    Add files via upload

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -3,12 +3,40 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/yanuarultfah/go-course/pkg/config"
 	"github.com/yanuarultfah/go-course/pkg/render"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
+type TemplateData struct {
+	StringMap map[string]string
+	IntMap    map[string]int
+	FloatMap  map[string]float32
+	Data      map[string]interface{}
+	CSRFToken string
+	flash     string
+	Warning   string
+	Error     string
+}
+
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "home.page.tmpl")
 }
-func About(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "about.page.tmpl")
 }
